ce1a437a85cff51c52146fb19873c0e23b4486bc
trieuld <trieule@wee.vn>
2023-02-04 20:01:16 +0700

    add SP_Get_CauHoi

--- internal/api/testPractice/pub.go
+++ internal/api/testPractice/pub.go
@@ -1,1 +1,11 @@
 package testpractice
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Reg(router *gin.Engine) {
+	//router.GET("/api/login/info", loginInfo)
+	//router.POST("/api/login/login", login)
+
+}
